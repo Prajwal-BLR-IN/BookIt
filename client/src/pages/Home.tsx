094d17c8bdd37777c9c01c3d3ddf7b658ca4c2a5
Prajwal <prajwalsjp2000@gmail.com>
2025-10-30 15:24:40 +0530

    feat: Integrate React Query (Tanstack Query) for data fetching and add API configuration

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,36 @@
-import { useEffect, useState } from "react";
-import { experiences } from "../assets/assets";
 import ExperienceCard from "../components/ExperienceCard";
 import type { Experience } from "../types/type";
+import { useQuery } from "@tanstack/react-query";
+import api from "../api/axios";
+import toast from "react-hot-toast";
+
+const fetchExperiences = async () => {
+  try {
+    const res = await api.get("/experiences");
+    return res.data;
+  } catch (error: any) {
+    console.log("Error fetching experiences", error.message);
+    toast.error(error.message);
+  }
+};
 
 const Home = () => {
-  const [experience, setExperience] = useState<Experience[]>([]);
-
-  useEffect(() => {
-    setExperience(experiences);
-  }, []);
+  const { data: experiences = [], isError } = useQuery({
+    queryKey: ["experience"],
+    queryFn: fetchExperiences,
+  });
 
   return (
-    <section className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {experience.map((exp, index) => (
-        <ExperienceCard key={exp._id} experience={exp} />
-      ))}
+    <section className="my-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {isError || !experiences || experiences.length === 0 ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          {experiences.map((exp: Experience, index: number) => (
+            <ExperienceCard key={exp._id} experience={exp} />
+          ))}
+        </>
+      )}
     </section>
   );
 };
